0c055f188c17e37a2ab26e04124d2df521603ccf
Sameer Giri <optimusam@users.noreply.github.com>
2017-11-25 04:55:13 +0000

    files uploaded

---
+++ main.js
@@ -0,0 +1,24 @@
+var randbtn = document.querySelector("#randbtn");
+var searchbtn = document.querySelector("#searchbtn");
+var search_box = document.querySelector("#query");
+var result_box = document.querySelector("#resultelem");
+var q;
+searchbtn.addEventListener("click", getResults);
+randbtn.addEventListener("click", function() {
+    window.open("https://en.wikipedia.org/wiki/Special:Random", "_blank");
+});
+
+function getResults() {
+    result_box.textContent="";
+    q = search_box.value;
+    var request = new XMLHttpRequest();
+    request.open("GET", "https://en.wikipedia.org/w/api.php?action=query&format=json&origin=*&prop=pageimages%7Cextracts&generator=search&formatversion=2&piprop=thumbnail&pithumbsize=50&pilimit=10&exchars=150&exlimit=10&exintro=1&explaintext=1&gsrsearch="+q+"&gsrlimit=10&gsrprop=snippet" , true);
+    request.onload = function () {
+        var data = JSON.parse(request.responseText);
+        var query_result = data.query.pages;
+        for(var el of query_result) {
+           result_box.insertAdjacentHTML("beforeend", "<a href=http://en.wikipedia.org/?curid=" + el.pageid + " target='_blank'><div><p class='search-title'>" + el.title + "</p><p class='snippet'>"+el.extract+"</p></div></a>")
+        }
+    }
+    request.send();
+}
